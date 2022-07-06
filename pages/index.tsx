be25fc3a45d7b09c2ec5e7a7ece17f7d7660d40d
antonijoDod <a.djodj@gmail.com>
2022-07-06 10:14:56 +0100

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,7 @@ import { Header, Main, Cards, Footer } from "@components";
 
 const Home: React.FC = () => {
     return (
-        <Flex direction="column" minH="100vh">
-            <Header />
-            <Main />
-            <Cards />
-            <Spacer />
-            <Footer />
-        </Flex>
+      
     );
 };
 
